77c631a18c5e205227c771312821ed571ec3215f
Balázs Marcell <KoegLike@proton.me>
2026-03-08 08:54:58 +0200

    added wasd

--- surface/player/player.gd
+++ surface/player/player.gd
@@ -44,7 +44,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Knockback
@@ -55,7 +55,7 @@ func _physics_process(delta: float) -> void:
 		knockback = Vector2.ZERO
 
 	# Mozgás
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction and knockback == Vector2.ZERO:
 		velocity.x = direction * SPEED
 		animated_sprite_2d.flip_h = direction < 0
